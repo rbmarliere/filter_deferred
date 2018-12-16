5015f59d8105e8613d6486325c529d06ec9d0d7d
Ricardo Marliere <rbmarliere@gmail.com>
2018-12-16 15:11:59 +0000

    fix deserialization

--- App.js
+++ App.js
@@ -1,9 +1,17 @@
 import EosApi from "eosjs-api";
-import Serializer from "./Serializer";
 import fs from "fs";
+import { Api, JsonRpc, RpcError, JsSignatureProvider, Serialize } from "eosjs";
+import fetch from "node-fetch";
+import { TextEncoder, TextDecoder } from "util";
 
 const options = { httpEndpoint: "https://api.eossweden.se", }
 const api = EosApi(options);
+
+const rpc = new JsonRpc("https://api.eossweden.se", { fetch });
+const signatureProvider = new JsSignatureProvider(["5JtUScZK2XEp3g9gh7F8bwtPTRAkASmNrrftmx4AxDKD5K4zDnr"]);
+const enc = new TextEncoder();
+const dec = new TextDecoder();
+const eos = new Api({ rpc, signatureProvider, textEncoder: enc, textDecoder: dec });
 
 const callback = (err, res) =>
 {
@@ -14,11 +22,16 @@ const callback = (err, res) =>
         api.getScheduledTransactions( { json: false, lower_bound: res.more }, callback );
 
     res.transactions.forEach(
-        (tx) => {
-            tx = Serializer.deserializeTransaction(tx.transaction);
+        async (tx) => {
+            tx = await
+                    eos.deserializeTransactionWithActions(
+                        Serialize.hexToUint8Array(tx.transaction));
+
+            tx = JSON.stringify(tx);
+
             fs.appendFile(
                 'transactions.txt',
-                "\n" + JSON.stringify(tx),
+                tx + "\n",
                 (err,data) => { if (err) { console.log(err); } }
             );
         }
